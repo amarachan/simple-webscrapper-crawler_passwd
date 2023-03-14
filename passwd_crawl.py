dd407a76a35caeab6c8f148dedd93afc588a2993
Amara_C <amara.cvg@gmail.com>
2023-03-14 02:18:25 +0000

    Update passwd_crawl.py

--- passwd_crawl.py
+++ passwd_crawl.py
@@ -1,27 +1,33 @@
 import requests
 import re
 
-def scrape_website(url):
+def scrape_website(url, output_file):
     response = requests.get(url)
     if response.status_code != 200:
-        print("Error: Failed to scrape website")
+        print(f"Error: Failed to scrape website {url}")
         return
 
     html = response.text
 
-    # search and extraction of usernames and passwords in the HTML
+    # Search for usernames and passwords in the HTML
     usernames = re.findall("(?i)username[=:]\s*(\S+)", html)
     passwords = re.findall("(?i)password[=:]\s*(\S+)", html)
 
-    if usernames:
-        print("Found usernames:")
-        for username in usernames:
-            print(username)
+    with open(output_file, "w") as f:
+        if usernames:
+            f.write("Found usernames:\n")
+            for username in usernames:
+                f.write(username + "\n")
 
-    if passwords:
-        print("Found passwords:")
-        for password in passwords:
-            print(password)
+        if passwords:
+            f.write("Found passwords:\n")
+            for password in passwords:
+                f.write(password + "\n")
 
-#usage
-scrape_website("https://mywebsite.com")
+    print(f"Output written to file: {output_file}")
+
+# Example usage
+if __name__ == "__main__":
+    url = input("Enter website URL: ")
+    output_file = input("Enter output file name: ")
+    scrape_website(url, output_file)
